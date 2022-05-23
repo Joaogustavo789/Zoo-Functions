c866e439ba8afb433f956ba191b1d8fde0fcc745
Joao Gustavo <jgustavomendonca@gmail.com>
2022-05-23 14:48:05 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return data.employees.find((em) => employeeName === em.firstName || employeeName === em.lastName);
 }
 
 module.exports = getEmployeeByName;
