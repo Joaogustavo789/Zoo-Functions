3333d7dedbf1311c1e0eb330d183e18999459ee1
Joao Gustavo <jgustavomendonca@gmail.com>
2022-05-24 18:24:13 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((acumulador, { name, residents }) => (
+      { ...acumulador, [name]: residents.length }
+    ), {});
+  }
+  const { specie, sex } = animal;
+  if (sex) {
+    return data.species.find(({ name }) => name === specie)
+      .residents.filter(({ sex: sexo }) => sexo === sex).length;
+  }
+  return data.species.find(({ name }) => name === specie).residents.length;
 }
 
 module.exports = countAnimals;
