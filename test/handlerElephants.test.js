69debe0fdee61b344c5e957379dfbab613042175
Joao Gustavo <jgustavomendonca@gmail.com>
2022-05-24 21:53:10 -0300

    Requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,13 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica se o argumento count retorna um número', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+  it('Verifica se o argumento names retorna um array de nomes que possui o nome Jeferson', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('Verifica se o argumento averageAge retornar um número aproximado', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+});
