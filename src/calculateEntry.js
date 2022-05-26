47a7129c1b3866a37389e59e80d2ebf0eff5de7f
Joao Gustavo <jgustavomendonca@gmail.com>
2022-05-26 12:01:38 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,36 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return entrants.reduce((acumulador, elemento) => {
+    if (elemento.age < 18) {
+      return {
+        ...acumulador,
+        child: acumulador.child + 1,
+      };
+    }
+    if (elemento.age >= 18 && elemento.age < 50) {
+      return {
+        ...acumulador,
+        adult: acumulador.adult + 1,
+      };
+    }
+    return {
+      ...acumulador,
+      senior: acumulador.senior + 1,
+    };
+  }, { child: 0, adult: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
+  if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  return (prices.child * child) + (prices.adult * adult) + (prices.senior * senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
